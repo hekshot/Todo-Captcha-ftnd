3dfec199c7c50837ccd12b28a5213884666982f4
hekshot <amankumarkr111@gmail.com>
2023-11-27 15:09:48 +0530

    login and signup ui changes

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,7 +4,19 @@ import { NavLink, NavLink as ReactLink, useNavigate } from "react-router-dom";
 import axios from "axios";
 import TextField from "@mui/material/TextField";
 import RotateRightIcon from "@mui/icons-material/RotateRight";
-
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  Container,
+  Form,
+  FormGroup,
+  Input,
+  Label,
+  Button,
+  Row,
+  Col,
+} from "reactstrap";
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -16,9 +28,8 @@ const Login = () => {
   const [refresh, setRefresh] = useState(false);
   const [random, setRandom] = useState(0);
 
-
   const randomNumberInRange = (min, max) => {
-    return Math.floor(Math.random()*(max-min+1)) + min;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   };
 
   useEffect(() => {
@@ -28,10 +39,12 @@ const Login = () => {
   }, [email, password, captcha]);
 
   useEffect(() => {
-    const randNum =randomNumberInRange(1,5000);
+    const randNum = randomNumberInRange(1, 5000);
     setRandom(randNum);
     const getImage = async () => {
-      const response = await axios.get(`http://localhost:8080/users/captcha/${randNum}`);
+      const response = await axios.get(
+        `http://localhost:8080/users/captcha/${randNum}`
+      );
       const imageUrl = response.data.imageUrl;
       setUrl(imageUrl);
     };
@@ -46,17 +59,20 @@ const Login = () => {
       return;
     }
     try {
-      const response = await axios.post(`http://localhost:8080/users/login/${random}`, {
-        userName: email,
-        userPassword: password,
-        captcha: captcha,
-      });
+      const response = await axios.post(
+        `http://localhost:8080/users/login/${random}`,
+        {
+          userName: email,
+          userPassword: password,
+          captcha: captcha,
+        }
+      );
 
       if (response.status === 200) {
-        navigate("/todos", { state: {userId: response.data.userId } });
-      } else if(response.message === "wrong"){
+        navigate("/todos", { state: { userId: response.data.userId } });
+      } else if (response.message === "wrong") {
         toast.error("wrong captcha");
-      }else{
+      } else {
         toast.error("Invalid E-mail or password");
       }
     } catch (error) {
@@ -64,97 +80,181 @@ const Login = () => {
       toast.error("Something went wrong");
     }
   };
-  
+
   return (
-    <div>
-      <div>
-        <div className="mt-4">
-          <div
-            style={{ marginLeft: "auto", marginRight: "auto", width: "50%" }}
-          >
-            <div style={{ backgroundColor: "dark", color: "white" }}>
+    // <div>
+    //   <div>
+    //     <div className="mt-4">
+    //       <div
+    //         style={{ marginLeft: "auto", marginRight: "auto", width: "50%" }}
+    //       >
+    //         <div style={{ backgroundColor: "dark", color: "white" }}>
+    //           <h3>Login here!!</h3>
+    //         </div>
+    //         <div>
+    //           <form>
+    //             <div>
+    //               <label htmlFor="email"> Enter your email</label>
+    //               <input
+    //                 type="text"
+    //                 placeholder="Enter email"
+    //                 id="email"
+    //                 value={email}
+    //                 onChange={(e) => setEmail(e.target.value)}
+    //               />
+    //             </div>
+    //             <div>
+    //               <label htmlFor="password"> Enter your password</label>
+    //               <input
+    //                 type="password"
+    //                 placeholder="Enter password"
+    //                 id="password"
+    //                 value={password}
+    //                 onChange={(e) => setPassword(e.target.value)}
+    //               />
+    //             </div>
+    //             <div>
+    //               <div
+    //                 style={{
+    //                   display: "flex",
+    //                   alignItems: "center",
+    //                   gap: "1rem",
+    //                 }}
+    //               >
+    //                 <TextField
+    //                   required
+    //                   margin="normal"
+    //                   fullWidth
+    //                   name="captcha"
+    //                   label="Enter captcha"
+    //                   type="text"
+    //                   id="captcha"
+    //                   onChange={(e) => {
+    //                     setCaptcha(e.target.value);
+    //                   }}
+    //                 />
+    //                 <img
+    //                   src={`data:image/png;base64,${url}`}
+    //                   alt="captcha"
+    //                   height="50rem"
+    //                   width="150rem"
+    //                   style={{ borderRadius: "1rem", marginTop: "0.6rem" }}
+    //                 />
+    //                 <RotateRightIcon
+    //                   onClick={() => setRefresh(!refresh)}
+    //                   style={{ marginTop: "0.6rem", fontSize: "2.5rem" }}
+    //                 />
+    //               </div>
+    //             </div>
+    //             <div className="text-center">
+    //               <button
+    //                 style={{
+    //                   outline: "none",
+    //                   color: "white",
+    //                   backgroundColor: "light",
+    //                 }}
+    //                 onClick={login}
+    //                 disabled={!formVerified}
+    //               >
+    //                 Login
+    //               </button>
+    //             </div>
+    //             <p className="mt-3 text-center">
+    //               Don't have an account?{" "}
+    //               <NavLink tag={ReactLink} to="/signup">
+    //                 Signup
+    //               </NavLink>
+    //             </p>
+    //           </form>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
+
+    <Container>
+      <Row className="mt-4">
+        <Col sm={{ size: 4, offset: 4 }}>
+          <Card color="dark" inverse>
+            <CardHeader>
               <h3>Login here!!</h3>
-            </div>
-            <div>
-              <form>
-                <div>
-                  <label htmlFor="email"> Enter your email</label>
-                  <input
+            </CardHeader>
+            <CardBody>
+              <Form>
+                <FormGroup>
+                  <Label for="email"> Enter your email</Label>
+                  <Input
                     type="text"
                     placeholder="Enter email"
                     id="email"
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
                   />
-                </div>
-                <div>
-                  <label htmlFor="password"> Enter your password</label>
-                  <input
+                </FormGroup>
+                <FormGroup>
+                  <Label for="password"> Enter your password</Label>
+                  <Input
                     type="password"
                     placeholder="Enter password"
                     id="password"
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
                   />
-                </div>
-                <div>
-                  <div
-                    style={{
-                      display: "flex",
-                      alignItems: "center",
-                      gap: "1rem",
+                </FormGroup>
+                <div
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    gap: "1rem",
+                  }}
+                >
+                  <TextField
+                    required
+                    margin="normal"
+                    fullWidth
+                    name="captcha"
+                    label="Enter captcha"
+                    type="text"
+                    id="captcha"
+                    style={{ backgroundColor: "white", borderRadius: "1rem" }}
+                    onChange={(e) => {
+                      setCaptcha(e.target.value);
                     }}
-                  >
-                    <TextField
-                      required
-                      margin="normal"
-                      fullWidth
-                      name="captcha"
-                      label="Enter captcha"
-                      type="text"
-                      id="captcha"
-                      onChange={(e) => {
-                        setCaptcha(e.target.value);
-                      }}
-                    />
-                    <img
-                      src={`data:image/png;base64,${url}`}
-                      alt="captcha"
-                      height="50rem"
-                      width="150rem"
-                      style={{ borderRadius: "1rem", marginTop: "0.6rem" }}
-                    />
-                    <RotateRightIcon
-                      onClick={() => setRefresh(!refresh)}
-                      style={{ marginTop: "0.6rem", fontSize: "2.5rem" }}
-                    />
-                  </div>
+                  />
+                  <img
+                    src={`data:image/png;base64,${url}`}
+                    alt="captcha"
+                    height="50rem"
+                    width="150rem"
+                    style={{ borderRadius: "1rem", marginTop: "0.6rem" }}
+                  />
+                  <RotateRightIcon
+                    onClick={() => setRefresh(!refresh)}
+                    style={{ marginTop: "0.6rem", fontSize: "2.5rem" }}
+                  />
                 </div>
-                <div className="text-center">
-                  <button
-                    style={{
-                      outline: "none",
-                      color: "white",
-                      backgroundColor: "light",
-                    }}
+                <Container className="text-center">
+                  <Button
+                    outline
+                    color="light"
                     onClick={login}
                     disabled={!formVerified}
                   >
                     Login
-                  </button>
-                </div>
+                  </Button>
+                </Container>
                 <p className="mt-3 text-center">
                   Don't have an account?{" "}
-                  <NavLink tag={ReactLink} to="/signup">
+                  <NavLink tag={ReactLink} to="/">
                     Signup
                   </NavLink>
                 </p>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+              </Form>
+            </CardBody>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -4,6 +4,19 @@ import React, { useEffect, useState } from "react";
 import { NavLink, NavLink as ReactLink, useNavigate } from "react-router-dom";
 import TextField from "@mui/material/TextField";
 import RotateRightIcon from "@mui/icons-material/RotateRight";
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  Container,
+  Form,
+  FormGroup,
+  Input,
+  Label,
+  Button,
+  Row,
+  Col,
+} from "reactstrap";
 
 const Signup = () => {
   const [email, setEmail] = useState("");
@@ -78,95 +91,179 @@ const Signup = () => {
   };
 
   return (
-    <div>
-      <div className="container">
-        <div className="row mt-4">
-          <div className="col-sm" style={{ width: "50%", margin: "0 auto" }}>
-            <div style={{ backgroundColor: "dark", color: "white" }}>
+    // <div>
+    //   <div className="container">
+    //     <div className="row mt-4">
+    //       <div className="col-sm" style={{ width: "50%", margin: "0 auto" }}>
+    //         <div style={{ backgroundColor: "dark", color: "white" }}>
+    //           <h3>Register here!!</h3>
+    //         </div>
+    //         <div>
+    //           <form>
+    //             <div className="form-group">
+    //               <label htmlFor="email"> Enter your email</label>
+    //               <input
+    //                 type="text"
+    //                 placeholder="Enter email"
+    //                 id="email"
+    //                 className="form-control"
+    //                 value={email}
+    //                 onChange={(e) => setEmail(e.target.value)}
+    //               />
+    //             </div>
+    //             <div className="form-group">
+    //               <label htmlFor="password"> Enter your password</label>
+    //               <input
+    //                 type="password"
+    //                 placeholder="Enter password"
+    //                 id="password"
+    //                 className="form-control"
+    //                 value={password}
+    //                 onChange={(e) => setPassword(e.target.value)}
+    //               />
+    //             </div>
+    //             <div>
+    //               <div
+    //                 style={{
+    //                   display: "flex",
+    //                   alignItems: "center",
+    //                   gap: "1rem",
+    //                 }}
+    //               >
+    //                 <TextField
+    //                   required
+    //                   margin="normal"
+    //                   fullWidth
+    //                   name="captcha"
+    //                   label="Enter captcha"
+    //                   type="text"
+    //                   id="captcha"
+    //                   onChange={(e) => {
+    //                     setCaptcha(e.target.value);
+    //                   }}
+    //                 />
+    //                 <img
+    //                   src={`data:image/png;base64,${url}`}
+    //                   alt="captcha"
+    //                   height="50rem"
+    //                   width="150rem"
+    //                   style={{ borderRadius: "1rem", marginTop: "0.6rem" }}
+    //                 />
+    //                 <RotateRightIcon
+    //                   onClick={() => setRefresh(!refresh)}
+    //                   style={{ marginTop: "0.6rem", fontSize: "2.5rem" }}
+    //                 />
+    //               </div>
+    //             </div>
+    //             <div className="text-center">
+    //               <button
+    //                 style={{
+    //                   outline: "none",
+    //                   color: "white",
+    //                   backgroundColor: "light",
+    //                 }}
+    //                 onClick={signUpToAccount}
+    //                 disabled={!formVerified}
+    //               >
+    //                 SignUp
+    //               </button>
+    //             </div>
+    //             <p className="mt-3 text-center">
+    //               Already have an account?{" "}
+    //               <NavLink tag={ReactLink} to="/login">
+    //                 Log In
+    //               </NavLink>
+    //             </p>
+    //           </form>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
+
+    <Container>
+      <Row className="mt-4">
+        <Col sm={{ size: 4, offset: 4 }}>
+          <Card color="dark" inverse>
+            <CardHeader>
               <h3>Register here!!</h3>
-            </div>
-            <div>
-              <form>
-                <div className="form-group">
-                  <label htmlFor="email"> Enter your email</label>
-                  <input
+            </CardHeader>
+            <CardBody>
+              <Form>
+                <FormGroup>
+                  <Label for="email"> Enter your email</Label>
+                  <Input
                     type="text"
                     placeholder="Enter email"
                     id="email"
-                    className="form-control"
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
                   />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="password"> Enter your password</label>
-                  <input
+                </FormGroup>
+                <FormGroup>
+                  <Label for="password"> Enter your password</Label>
+                  <Input
                     type="password"
                     placeholder="Enter password"
                     id="password"
-                    className="form-control"
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
                   />
-                </div>
-                <div>
-                  <div
-                    style={{
-                      display: "flex",
-                      alignItems: "center",
-                      gap: "1rem",
+                </FormGroup>
+                <div
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    gap: "1rem",
+                  }}
+                >
+                  <TextField
+                    required
+                    margin="normal"
+                    fullWidth
+                    name="captcha"
+                    label="Enter captcha"
+                    type="text"
+                    id="captcha"
+                    style={{ backgroundColor: "white", borderRadius: "1rem" }}
+                    onChange={(e) => {
+                      setCaptcha(e.target.value);
                     }}
-                  >
-                    <TextField
-                      required
-                      margin="normal"
-                      fullWidth
-                      name="captcha"
-                      label="Enter captcha"
-                      type="text"
-                      id="captcha"
-                      onChange={(e) => {
-                        setCaptcha(e.target.value);
-                      }}
-                    />
-                    <img
-                      src={`data:image/png;base64,${url}`}
-                      alt="captcha"
-                      height="50rem"
-                      width="150rem"
-                      style={{ borderRadius: "1rem", marginTop: "0.6rem" }}
-                    />
-                    <RotateRightIcon
-                      onClick={() => setRefresh(!refresh)}
-                      style={{ marginTop: "0.6rem", fontSize: "2.5rem" }}
-                    />
-                  </div>
+                  />
+                  <img
+                    src={`data:image/png;base64,${url}`}
+                    alt="captcha"
+                    height="50rem"
+                    width="150rem"
+                    style={{ borderRadius: "1rem", marginTop: "0.6rem" }}
+                  />
+                  <RotateRightIcon
+                    onClick={() => setRefresh(!refresh)}
+                    style={{ marginTop: "0.6rem", fontSize: "2.5rem" }}
+                  />
                 </div>
-                <div className="text-center">
-                  <button
-                    style={{
-                      outline: "none",
-                      color: "white",
-                      backgroundColor: "light",
-                    }}
+                <Container className="text-center">
+                  <Button
+                    outline
+                    color="light"
                     onClick={signUpToAccount}
                     disabled={!formVerified}
                   >
-                    SignUp
-                  </button>
-                </div>
+                    Signup
+                  </Button>
+                </Container>
                 <p className="mt-3 text-center">
-                  Already have an account?{" "}
+                Already have an account?{" "}
                   <NavLink tag={ReactLink} to="/login">
                     Log In
                   </NavLink>
                 </p>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+              </Form>
+            </CardBody>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
